8ebc62f03e4b11838b9d5ed3ded22ec6f4b3ed78
Noah <business.noah@hotmail.com>
2022-12-05 14:35:57 -0700

    commit tree 9c63d6f93ed7fffe6a15335e32303a06acf5599e

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "NOAH is the champion";
+document.body.append("h1");
